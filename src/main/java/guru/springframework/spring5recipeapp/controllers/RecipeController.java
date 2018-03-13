3eac75968544eef6604fa8e5bd42930015ab3773
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2018-03-13 09:29:18 +0100

    exception handling

--- src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.springframework.spring5recipeapp.controllers;
 
 import guru.springframework.spring5recipeapp.commands.RecipeCommand;
+import guru.springframework.spring5recipeapp.exceptions.NotFoundException;
 import guru.springframework.spring5recipeapp.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -57,5 +60,35 @@ public class RecipeController {
 		recipeService.deleteById(Long.valueOf(id));
 
 		return "redirect:/";
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound(Exception e) {
+
+		log.error("Handling not found exception");
+		log.error(e.getMessage());
+
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.setViewName("404error");
+		modelAndView.addObject("exception", e);
+
+		return modelAndView;
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(NumberFormatException.class)
+	public ModelAndView handleNumberFOrmatException(Exception e) {
+
+		log.error("Handling number format exception");
+		log.error(e.getMessage());
+
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.setViewName("400error");
+		modelAndView.addObject("exception", e);
+
+		return modelAndView;
 	}
 }
--- src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -56,7 +56,18 @@ public class RecipeControllerTest {
 		when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
 		mockMvc.perform(get("/recipe/1/show"))
-				.andExpect(status().isNotFound());
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("404error"));
+	}
+
+	@Test
+	public void testGetRecipeNumberFormatException() throws Exception {
+
+		//when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+		mockMvc.perform(get("/recipe/asdf/show"))
+				.andExpect(status().isBadRequest())
+				.andExpect(view().name("400error"));
 	}
 
 	@Test
